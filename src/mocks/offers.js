effde564945c8b18c86ec7746addd9144945ca5a
Dmitry Gusev <dvgusev@hotmail.com>
2020-02-24 13:07:40 +0300

    Added offers mock

---
+++ src/mocks/offers.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    title: 'Apartment 1',
+    price: 100,
+    pricePeriod: 'night',
+    thumnnailUrl: '/img/apartment-01.jpg',
+    objectType: 'apartment',
+    badgeText: null
+  },
+  {
+    title: 'Apartment 2',
+    price: 200,
+    pricePeriod: 'night',
+    thumnnailUrl: '/img/apartment-02.jpg',
+    objectType: 'apartment',
+    badgeText: 'VIP'
+  },
+  {
+    title: 'Apartment 3',
+    price: 250,
+    pricePeriod: 'night',
+    thumnnailUrl: '/img/apartment-03.jpg',
+    objectType: 'apartment',
+    badgeText: 'Premium'
+  },
+  {
+    title: 'Apartment 4',
+    price: 199,
+    pricePeriod: 'night',
+    thumnnailUrl: '/img/apartment-04.jpg',
+    objectType: 'apartment',
+    badgeText: null,
+  }
+]
