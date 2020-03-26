2210ebad947d0827270897801082cb20edb55c1b
Dmitry Gusev <dvgusev@hotmail.com>
2020-03-26 22:29:34 +0300

    Removed mock store from offer-detail test

--- src/components/offer-detail/offer-detail.test.js
+++ src/components/offer-detail/offer-detail.test.js
@@ -1,31 +1,18 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import OfferDetail from "./offer-detail.jsx";
+import {OfferDetail} from "./offer-detail.jsx";
 import {BrowserRouter} from 'react-router-dom';
-import configureStore from "redux-mock-store";
-import {Provider} from "react-redux";
-import Offers from '../../test-mocks/offers.js';
-
-const mockStore = configureStore([]);
-
-const store = mockStore({
-  offers: {
-    data: Offers,
-    isLoading: false,
-    isError: false,
-  }
-});
+import offers from '../../test-mocks/offers.js';
 
 it(`Should Main component render correctly`, () => {
 
   const tree = renderer.create(
-      <Provider store={store}>
-        <BrowserRouter>
-          <OfferDetail
-            offerId={`1`}
-          />
-        </BrowserRouter>
-      </Provider>,
+      <BrowserRouter>
+        <OfferDetail
+          offerId={`1`}
+          offer={offers[0]}
+        />
+      </BrowserRouter>,
       {
         createNodeMock: () => {
           return document.createElement(`section`);
